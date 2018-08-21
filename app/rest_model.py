49f33ffe76c62705fffa62fc16eb3e0150b06647
Mike Ellis <mtcanuck1@gmail.com>
2018-08-20 21:39:40 -0400

    added site and node classes
    Site and node classes facilitate the link of common names to database id's

--- app/rest_model.py
+++ app/rest_model.py
@@ -115,3 +115,40 @@ class CNC:
                 if (axis.number < temp_axis.number):
                     index += 1
             self.axis.insert(index, temp_axis)
+
+##################################################
+# Node Class
+
+class Node:
+    def __init__(self, site):
+        self.id = site["id"]
+        self.name = site["name"]
+        self.controller_id = site["controller_id"]
+        self.level = site["level"]
+        self.position = site["position"]
+        self.parent_id = site["parent_id"]
+        self.child_id = site["child_id"]
+        self.tag_id = site["tag_id"]
+        
+    def is_type(self):
+        ret = ""
+        if isinstance(self.controller_id, str) and len(self.controller_id) > 0 :
+            data = json_from_path(["class","controller","instance",self.controller_id,"relations"])
+            ret = list(data)[0]
+        return ret
+        
+        
+##################################################
+# Site Class
+
+class Site:
+    def __init__(self):
+        self.nodes = []
+        response = requests.get(SITE)
+        jresp = json.loads(response.text)
+        for node in jresp:
+            temp_node = Node(node)
+            self.nodes.append(temp_node)
+
+
+        
